bb0e2ecf0bb7cb79a11a880f3d28c42e5961659b
Kyle McCarthy <km@kylemccarthy.io>
2016-02-02 20:53:54 -0600

    base authentication routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * Authentication routes
+ */
+// Authentication Routes...
+$this->get('login', 'Auth\AuthController@showLoginForm');
+$this->post('login', 'Auth\AuthController@login');
+$this->get('logout', 'Auth\AuthController@logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\AuthController@showRegistrationForm');
+$this->post('register', 'Auth\AuthController@register');
+
+// Password Reset Routes...
+$this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+$this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+$this->post('password/reset', 'Auth\PasswordController@reset');
+
+/**
+ * Group all the routes together and make them use the api guard.  Prefix them with api/{version_number}
+ */
 $router->group(['prefix' => 'api/v1', 'middleware' => 'auth:api'], function() {
+
 
 });
