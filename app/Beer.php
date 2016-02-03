0969584e1bd7dcf57b60a19ada51b4518f6a3238
Kyle McCarthy <km@kylemccarthy.io>
2016-02-02 20:13:15 -0600

    brewery and beer relationship

--- app/Beer.php
+++ app/Beer.php
@@ -9,4 +9,13 @@ class Beer extends Model
     protected $table = 'beers';
     protected $fillable = ['name', 'brewery_id', 'category_id', 'style_id', 'body', 'sweetness', 'color_id',
         'abv', 'ibu', 'hoppiness', 'maltiness', 'description'];
+
+    /**
+     * Get the brewery that the beer belongs to
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brewery()
+    {
+        return $this->belongsTo('App\Brewery');
+    }
 }
--- app/Brewery.php
+++ app/Brewery.php
@@ -8,4 +8,13 @@ class Brewery extends Model
 {
     protected $table = 'breweries';
     protected $fillable = ['name', 'location'];
+
+    /**
+     * Get the beers that belong to the brewery
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function beers()
+    {
+        return $this->hasMany('App\Beer');
+    }
 }
