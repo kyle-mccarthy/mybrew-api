6617600b84c4e66b3ebe9374d97aa44de2019ae2
Kyle McCarthy <km@kylemccarthy.io>
2016-02-16 18:39:29 -0600

    wiped web auth - going to replace with api auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,19 +3,7 @@
 /**
  * Authentication routes
  */
-// Authentication Routes...
-$this->get('login', 'Auth\AuthController@showLoginForm');
-$this->post('login', 'Auth\AuthController@login');
-$this->get('logout', 'Auth\AuthController@logout');
 
-// Registration Routes...
-$this->get('register', 'Auth\AuthController@showRegistrationForm');
-$this->post('register', 'Auth\AuthController@register');
-
-// Password Reset Routes...
-$this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
-$this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
-$this->post('password/reset', 'Auth\PasswordController@reset');
 
 /**
  * Group all the routes together and make them use the api guard.  Prefix them with api/{version_number}
