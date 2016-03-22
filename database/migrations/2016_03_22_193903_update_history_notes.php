cdb1d756c793cd4fabe1714a3f0ab707c259eaeb
Kyle McCarthy <km@kylemccarthy.io>
2016-03-22 14:44:38 -0500

    allow nullable notes on histories table

---
+++ database/migrations/2016_03_22_193903_update_history_notes.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateHistoryNotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('histories', function (Blueprint $table) {
+            $table->text('notes')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
